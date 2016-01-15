363acac466dab14f4ac5a2f931aef3922e289f6d
sachalmalick <sachalmalick@gmail.com>
2016-01-14 21:33:38 -0500

    added jcurses key listener

---
+++ He/Test.java
@@ -0,0 +1,22 @@
+import jcurses.system.CharColor;
+import jcurses.widgets.DefaultLayoutManager;
+import jcurses.widgets.Label;
+import jcurses.widgets.WidgetsConstants;
+import jcurses.widgets.Window;
+import jcurses.system.InputChar;
+import jcurses.system.Toolkit;
+
+/**
+ * Simple example in JCurses
+ *
+ * @package hell.cenobites.code.java.jcurses.examples.one
+ * @author Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
+ * @version 1.0
+ */
+public class Test {    
+        public static void main(String[] args) throws Exception {
+            
+        System.out.println(Toolkit.readCharacter());
+
+}
+}
